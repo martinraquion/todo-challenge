2debe593c4df088b8e5d334db7c9572d23c8b11c
Martin Raquion <martin@boom.camp>
2019-06-03 23:37:07 +0800

    Added Filtering of Progress

--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     var list = [];
     // Adding Task
+    $('button').attr('disabled',true)
+    $('button').addClass('disabled')
+
+   $('.text-content').keyup(function(){
+    $('button').prop('disabled', false)
+    $('button').css('background', '#D97777')
+})
+
     $('.input-button').click(function() {
         var newTask = $('.text-content').val()
         list.push(newTask)
@@ -12,7 +20,7 @@ $(document).ready(function() {
                   
                     <div class="title">${newTask}</div>
                      <div class="icon-wrap">
-                        <span class="progress">New</span>
+                        <span class="progress new">New</span>
                         <span class="fas fa-trash"></span>
                     </div>
                 </div>
@@ -20,58 +28,64 @@ $(document).ready(function() {
         ` 
         $('.list-container').prepend(addTask)
 
-        $('.icon-wrap').on('click', '.progress',function(){
+        $('.icon-wrap').on('click', '.progress', function(){
+            $(this).toggleClass('new')
             $(this).toggleClass('in-progress')
             $(this).text($(this).text() == 'New' ? 'In Progress' : 'New');
+       
         })  
     
         //Deleting Task
         $('.list-content').on('click', '.fa-trash', function(){
         $(this).parent().parent().parent().remove()
-         })  
+
+        for (let x=0;x<=list.length;x++ ){
+            if(list[x]===$(this).parent().prev().text()){
+                list.splice(x,1);
+            }
+            // console.log(list)
+        }
+        })  
 
         $('.fa-circle').on('click', function(){
          $(this).next().toggleClass('done-text')
          $(this).toggleClass('done-color')
         //  $(this).next().next().child().hasClass('progress').text('Done')
-        })
-      
+          $(this).next().next().children().first().toggleClass('hidden');
+           console.log($(this).next().next().toggleClass('align-right'))
+    })
+
+        $('.text-content').val('')
+        $('button').addClass('disabled')
     });
 
-
-        $('.icon-wrap').on('click', '.progress',function(){
-        $(this).toggleClass('in-progress')
-        $(this).text($(this).text() == 'New' ? 'In Progress' : 'New');
-         })  
-    
-        //Deleting Task
-        $('.list-content').on('click', '.fa-trash', function(){
-        $(this).parent().parent().parent().remove()
-         })  
-
-        $('.fa-circle').on('click', function(){
-         $(this).next().toggleClass('done-text')
-         $(this).toggleClass('done-color')
-        })
-
-    
     //Filter
     $('.filter-all').on('click', function(){
         $('.filter-done').removeClass('selected');
         $('.filter-prog').removeClass('selected');
         $('.filter-all').addClass('selected')
+        if($('.progress').hasClass('in-progress')){
+            $('.new').parent().parent().parent().removeClass('hidden');
+            // $('.progress').removeClass('new')
+        }
+        $('.title:not(".done-text")').parent().parent().removeClass('hidden')
     })
 
     $('.filter-prog').on('click', function(){
         $('.filter-all').removeClass('selected');
         $('.filter-done').removeClass('selected');
-        $('.filter-prog').addClass('selected')
+        $('.filter-prog').addClass('selected');
+        if($('.progress').hasClass('in-progress')){
+            $('.new').parent().parent().parent().addClass('hidden');
+            // $('.progress').removeClass('new')
+        }
     })
 
     $('.filter-done').on('click', function(){
         $('.filter-all').removeClass('selected');
         $('.filter-prog').removeClass('selected');
         $('.filter-done').addClass('selected')
+        $('.title:not(".done-text")').parent().parent().addClass('hidden')
     })
 
 });
